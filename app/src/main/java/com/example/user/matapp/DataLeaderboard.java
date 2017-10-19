d439b5b1c9295bdd79ab92db7776cf079fc99c8f
sjupiyandi <sjupiyandi@gmail.com>
2017-10-19 12:21:21 +0700

    terbaru leaderboard

---
+++ app/src/main/java/com/example/user/matapp/DataLeaderboard.java
@@ -0,0 +1,70 @@
+package com.example.user.matapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DataLeaderboard extends RecyclerView.Adapter<DataLeaderboard.ViewHolder> {
+    private ArrayList<String> countries;
+    private ArrayList gambar;
+    private ArrayList<String> no;
+    private ArrayList<String> lcertificate;
+    private ArrayList<String> lmedal;
+    private ArrayList<String> lcoins;
+
+    public DataLeaderboard(ArrayList<String> countries, ArrayList gambar,ArrayList<String> no,ArrayList<String> lcertificate,ArrayList<String> lmedal, ArrayList<String> lcoins) {
+        this.gambar = gambar;
+        this.countries = countries;
+        this.no = no;
+        this.lcertificate = lcertificate;
+        this.lmedal = lmedal;
+        this.lcoins = lcoins;
+    }
+
+    @Override
+    public DataLeaderboard.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_leaderboad, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataLeaderboard.ViewHolder viewHolder, int i) {
+        viewHolder.iv_gambar.setImageResource((int) gambar.get(i));
+        viewHolder.tv_country.setText(countries.get(i));
+        viewHolder.no.setText(no.get(i));
+        viewHolder.lcertificate.setText(lcertificate.get(i));
+        viewHolder.lmedal.setText(lmedal.get(i));
+        viewHolder.lcoins.setText(lcoins.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tv_country;
+        private TextView no;
+        private TextView lcertificate;
+        private TextView lmedal;
+        private TextView lcoins;
+        private ImageView iv_gambar;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            tv_country = (TextView)view.findViewById(R.id.teksMenu);
+            iv_gambar = (ImageView) view.findViewById(R.id.gambarIcon);
+            lcertificate = (TextView)view.findViewById(R.id.label_certificate);
+            no = (TextView)view.findViewById(R.id.no);
+            lmedal = (TextView)view.findViewById(R.id.label_medal);
+            lcoins = (TextView)view.findViewById(R.id.label_coin);
+        }
+    }
+
+}
--- app/src/main/java/com/example/user/matapp/LeaderboardAsli.java
+++ app/src/main/java/com/example/user/matapp/LeaderboardAsli.java
@@ -34,6 +34,7 @@ public class LeaderboardAsli extends AppCompatActivity implements View.OnClickLi
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        initViews();
 
 
 
@@ -50,4 +51,154 @@ public class LeaderboardAsli extends AppCompatActivity implements View.OnClickLi
         onBackPressed();
         return true;
     }
+
+
+    private ArrayList countries;
+    private ArrayList gambar;
+    private ArrayList no;
+    private ArrayList lcertificate;
+    private ArrayList lmedal;
+    private ArrayList lcoins;
+
+    private void initViews(){
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.leader_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        no = new ArrayList<>();
+        no.add("1");
+        no.add("2");
+        no.add("3");
+        no.add("4");
+        no.add("5");
+        no.add("6");
+        no.add("7");
+
+        countries = new ArrayList<>();
+        countries.add("Sundar Pichai");
+        countries.add("Van Persie");
+        countries.add("Ibrahimovic");
+        countries.add("Wayne Rooney");
+        countries.add("Mesut Ozil");
+        countries.add("C.Ronaldo");
+        countries.add("Lionel Messi");
+
+
+
+        gambar = new ArrayList<>();
+        gambar.add(R.drawable.sundar);
+        gambar.add(R.drawable.van);
+        gambar.add(R.drawable.ibrahimovic);
+        gambar.add(R.drawable.rooney);
+        gambar.add(R.drawable.ozil);
+        gambar.add(R.drawable.ronaldo);
+        gambar.add(R.drawable.messi);
+
+
+        lcertificate = new ArrayList<>();
+        lcertificate.add("1");
+        lcertificate.add("1");
+        lcertificate.add("1");
+        lcertificate.add("1");
+        lcertificate.add("1");
+        lcertificate.add("1");
+        lcertificate.add("1");
+
+        lmedal = new ArrayList<>();
+        lmedal.add("5");
+        lmedal.add("5");
+        lmedal.add("4");
+        lmedal.add("4");
+        lmedal.add("3");
+        lmedal.add("3");
+        lmedal.add("2");
+
+        lcoins = new ArrayList<>();
+        lcoins.add("10000");
+        lcoins.add("8000");
+        lcoins.add("7000");
+        lcoins.add("6000");
+        lcoins.add("5000");
+        lcoins.add("4000");
+        lcoins.add("3000");
+
+
+
+        RecyclerView.Adapter adapter = new DataLeaderboard(countries, gambar,no,lcertificate,lmedal,lcoins);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if(child != null && gestureDetector.onTouchEvent(e)) {
+                    int position = rv.getChildAdapterPosition(child);
+                    mulaiSubMenu(position);
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+
+
+
+
+    public void mulaiSubMenu(int position){
+        Intent intent;
+        switch (position) {
+            case 0:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+
+                break;
+            case 1:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+
+                break;
+            case 2:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+                break;
+            case 3 :
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+                break;
+            case 4 :
+                intent = new Intent(getApplicationContext(), Ozil.class);
+                startActivity(intent);
+                break;
+            case 5:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+                break;
+            case 6:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), (CharSequence) countries.get(position), Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+
+    }
+
+
 }
+
